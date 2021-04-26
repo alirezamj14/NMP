7f9d0323ff5a80844eca012f1127d6dbc264f279
s-a-n-d-y <mr.sandipan.das@gmail.com>
2021-04-26 21:01:11 +0200

    Patch extraction and training data creation with mask

--- test_masked_train.py
+++ test_masked_train.py
@@ -39,29 +39,46 @@ def patch_filter_indices(rows=28, cols=28, radius=3, pad_width=0):
     
     return all_idx
 
-def debug_filter(image, indices):
-    rows, cols, _ = indices.shape
-    plt.ion() # turn on interactive mode
-    for row in range(0,rows):
-        for col in range(0,cols):
-            mask = np.zeros(image.shape)
+def debug_filter(images, mask):
+    
+    for i in range(images.shape[1]):
+        image = images[:,i]
+        overlay = np.zeros(image.shape)
+        feature_image = np.repeat(overlay[..., np.newaxis], 3, -1)
+        feature_image[:,0] = mask[:,i]
+        feature_image = np.reshape(feature_image,(28,28,3))
+        feature_image = Image.fromarray((feature_image * 255).astype(np.uint8))
+        image_ = image.T
+        image_ = np.reshape(image_,(28,28))
+        image_ = Image.fromarray((image_ * 255).astype(np.uint8))
+
+        plt.imshow(image_)
+        plt.imshow(feature_image, 'viridis', interpolation='nearest', alpha=0.6)
+        plt.ion() # turn on interactive mode
+        plt.show()
+        plt.pause(0.01)
+        plt.clf()
+    
+def train_patched_data(X_train, X_test, all_idx, rows=28, cols=28, debug=False):
+    train_len = X_train.shape[1]
+    test_len = X_test.shape[1]
+    
+    # Since, stride = 0 and padding = 0, Row len = rows-2 and Col len = cols - 2
+    for row in range(0,rows-2):
+        for col in range(0,cols-2):
+            mask = np.zeros(rows*cols)
+            mask[all_idx[row,col]] = 1
+            train_mask = np.repeat(mask[..., np.newaxis],train_len, -1)
+            test_mask = np.repeat(mask[..., np.newaxis],test_len, -1)
+
+            curr_x_train = X_train*train_mask
+            curr_x_test = X_test*test_mask
+
+            if debug == True:
+                debug_filter(X_train, train_mask)
             
-            feature_image = np.repeat(mask[..., np.newaxis], 3, -1)
+            # Now train SSFN with NMP here
 
-            mask[indices[row,col]] = 1
-            feature_image[:,0] = mask
-            
-            feature_image = np.reshape(feature_image,(28,28,3))
-            feature_image = Image.fromarray((feature_image * 255).astype(np.uint8))
-
-            image_ = image.T
-            image_ = np.reshape(image_,(28,28))
-            image_ = Image.fromarray((image_ * 255).astype(np.uint8))
-
-            plt.imshow(image_)
-            plt.imshow(feature_image, 'viridis', interpolation='nearest', alpha=0.6)
-            plt.show()
-            _ = input("Press [enter] to continue.")
 
 X_train =  loadmat("./mat_files/MNIST.mat")["train_x"].astype(np.float32)
 X_test =  loadmat("./mat_files/MNIST.mat")["test_x"].astype(np.float32)
@@ -74,7 +91,7 @@ input_shape = (28, 28, 1)
 # All flattened masks indices
 all_idx = patch_filter_indices(rows=28, cols=28, radius=3)
 
-debug_filter(X_train.T[20,:], all_idx)
+train_patched_data (X_train[:,1:3], X_test, all_idx, debug=True)
 
 '''
 model = CNNModel(num_classes, input_shape)
